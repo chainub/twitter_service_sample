819e4da4381b061519c065925e11d5e91a6bee48
yuma300 <ywakimoto2s@gmail.com>
2015-05-06 11:05:19 +0900

    integrating tweet

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,10 +1,14 @@
 module Api::V1
   class ApiController < ::ApplicationController
     def chainub_action
-      SlackBot.notify(
-        body: params['body'], 
-        bot_name: "chainub-bot"
-      )
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key = YOUR_CONSUMER_KEY
+        config.consumer_secret = YOUR_COSUMER_SECRET
+        config.access_token = YOUR_ACCESS_TOKEN
+        config.access_token_secret = YOUR_ACCESS_TOKEN_SECRET
+      end 
+
+      client.update(params['body'])
       render json: { "success" => true }
     end
   end
